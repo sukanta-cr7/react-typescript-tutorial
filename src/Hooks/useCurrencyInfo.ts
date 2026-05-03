5dba0d3223925cd6a2776a9dec556356ebf3e125
Sukanta Majhi <sukantamajhi777@gmail.com>
2026-05-03 10:52:08 +0530

    bugs fixes on currency converter

--- src/Hooks/useCurrencyInfo.ts
+++ src/Hooks/useCurrencyInfo.ts
@@ -1,9 +1,13 @@
 import { useState, useEffect } from "react";
 
 function useCurrencyInfo(currency : string) {
-    const [data, setData] = useState({})
+    const [data, setData] = useState<Record<string, number>>({})
+    
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        const date = new Date()
+        const dateStr = date.toISOString().split("T")[0]    //format to YYYY-MM-DD
+        
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@${dateStr}/v1/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
         console.log(data)
